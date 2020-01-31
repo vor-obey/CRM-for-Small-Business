31bfd33b676abc1b278a4753202657aa3be2ce57
Iraklii Makarenko <iraklii.makarenko@gmail.com>
2020-01-31 20:33:36 +0200

    heroku setup

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const USER_URLS = {
     USERS: `/users`,
     ORDERS: `/orders`,
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: process.env.REACT_APP_BASE_URL
         })
     }
-}
+};
