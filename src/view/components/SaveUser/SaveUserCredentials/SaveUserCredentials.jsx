f0b9bbcb9d77b3b52383dcaeca200b76dfb1a155
borisimo <borisimoprogrammerjs@gmail.com>
2020-03-02 18:26:36 +0200

    fix bug in create-user || validate password

--- src/view/components/SaveUser/SaveUserCredentials/SaveUserCredentials.jsx
+++ src/view/components/SaveUser/SaveUserCredentials/SaveUserCredentials.jsx
@@ -1,85 +1,92 @@
 import React from 'react';
-import {FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField} from "@material-ui/core";
+import {
+   FormControl,
+   Grid,
+   IconButton,
+   InputAdornment,
+   InputLabel,
+   OutlinedInput,
+   TextField
+} from "@material-ui/core";
 import {Visibility, VisibilityOff} from "@material-ui/icons";
 
 export const SaveUserCredentials = (props) => {
 
-    const {
-        onChangedInput,
-        credentials,
-        showPassword,
-        toggleShowPassword
-    } = props;
+   const {
+      onChangedInput,
+      credentials,
+      showPassword,
+      toggleShowPassword
 
-    return (
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <TextField
-                    label={"Email Address"}
-                    name={"email"}
-                    variant={"outlined"}
-                    type="email"
-                    value={credentials.email}
-                    onChange={onChangedInput}
-                    required
-                    fullWidth
-                />
-            </Grid>
-            <Grid item sm={6} xs={12}>
-                <FormControl variant="outlined" required fullWidth>
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
-                        label={"Password"}
-                        name={"password"}
-                        type={showPassword ? 'text' : 'password'}
-                        value={credentials.password}
-                        onChange={onChangedInput}
-                        labelWidth={85}
-                        fullWidth
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={toggleShowPassword}
-                                    size='small'
-                                    edge='end'>
-                                    {showPassword ?
-                                        <Visibility fontSize='small'/>
-                                        : <VisibilityOff fontSize='small'/>
-                                    }
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-            </Grid>
-            <Grid item sm={6} xs={12}>
-                <FormControl variant="outlined" required fullWidth>
-                    <InputLabel htmlFor="outlined-adornment-password">Repeat Password</InputLabel>
-                    <OutlinedInput
-                        label={"Password"}
-                        name={"confirmPassword"}
-                        placeholder={"Repeat Password *"}
-                        type={showPassword ? 'text' : 'password'}
-                        value={credentials.confirmPassword}
-                        onChange={onChangedInput}
-                        labelWidth={200}
-                        required
-                        fullWidth
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={toggleShowPassword}
-                                    size='small'
-                                    edge='end'>
-                                    {showPassword ? <Visibility fontSize='small'/> : <VisibilityOff fontSize='small'/>}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-            </Grid>
-        </Grid>
-    )
+   } = props;
+
+   return (
+      <Grid container spacing={2}>
+         <Grid item xs={12}>
+            <TextField
+               label={"Email Address"}
+               name={"email"}
+               variant={"outlined"}
+               type="email"
+               value={credentials.email}
+               onChange={onChangedInput}
+               required
+               fullWidth
+            />
+         </Grid>
+         <Grid item sm={6} xs={12}>
+            <FormControl variant="outlined" required fullWidth>
+               <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+               <OutlinedInput
+                  label={"Password"}
+                  name={"password"}
+                  type={showPassword ? 'text' : 'password'}
+                  value={credentials.password}
+                  onChange={onChangedInput}
+                  labelWidth={110}
+                  fullWidth
+                  endAdornment={
+                     <InputAdornment position="end">
+                        <IconButton
+                           aria-label="toggle password visibility"
+                           onClick={toggleShowPassword}
+                           size='small'
+                           edge='end'>
+                           {showPassword ?
+                              <Visibility fontSize='small'/>
+                              : <VisibilityOff fontSize='small'/>
+                           }
+                        </IconButton>
+                     </InputAdornment>
+                  }
+               />
+            </FormControl>
+         </Grid>
+         <Grid item sm={6} xs={12}>
+            <FormControl variant="outlined" required fullWidth>
+               <InputLabel htmlFor="outlined-adornment-password">Repeat Password</InputLabel>
+               <OutlinedInput
+                  name={"confirmPassword"}
+                  type={showPassword ? 'text' : 'password'}
+                  value={credentials.confirmPassword}
+                  onChange={onChangedInput}
+                  labelWidth={150}
+                  required
+                  fullWidth
+                  endAdornment={
+                     <InputAdornment position="end">
+                        <IconButton
+                           aria-label="toggle password visibility"
+                           onClick={toggleShowPassword}
+                           size='small'
+                           edge='end'>
+                           {showPassword ? <Visibility fontSize='small'/> : <VisibilityOff fontSize='small'/>}
+                        </IconButton>
+                     </InputAdornment>
+                  }
+               />
+            </FormControl>
+         </Grid>
+      </Grid>
+   )
 };
--- src/view/components/SaveUser/SaveUserForm.jsx
+++ src/view/components/SaveUser/SaveUserForm.jsx
@@ -4,124 +4,133 @@ import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import {saveUserStyle} from './SaveUser.style';
 import {SaveUserCredentials} from "./SaveUserCredentials/SaveUserCredentials";
 import {SaveUserDetails} from "./SaveUserDetails/SaveUserDetails";
+import {COMMON_ERROR_MESSAGE_VALIDATE} from "../../../constants/statuses";
+import {useDispatch} from "react-redux";
+import {setSnackBarStatus} from "../../../data/store/auxiliary/auxiliaryActions";
 
 const useStyles = makeStyles(saveUserStyle);
 
 export const SaveUserForm = (props) => {
-    const {
-        userDetails,
-        title,
-        buttonText,
-        isEdit,
-        roles,
-        onSubmit,
-        currentUser,
-    } = props;
+   const {
+      userDetails,
+      title,
+      buttonText,
+      isEdit,
+      roles,
+      onSubmit,
+      currentUser,
+   } = props;
 
-    const classes = useStyles();
+   const classes = useStyles();
 
-    const [userDetailsInputs, setUserDetailsInputs] = useState({
-        firstName: '',
-        lastName: '',
-        middleName: '',
-        contactNumber: '',
-        roleId: ''
-    });
-    const [userCredentials, setUserCredentials] = useState({
-        email: '',
-        password: '',
-        confirmPassword: '',
-    });
-    const [showPassword, setShowPassword] = useState(false);
+   const dispatch = useDispatch();
 
-    useEffect(() => {
-        setUserDetailsInputs(userDetails);
-    }, [userDetails]);
+   const [userDetailsInputs, setUserDetailsInputs] = useState({
+      firstName: '',
+      lastName: '',
+      middleName: '',
+      contactNumber: '',
+      roleId: ''
+   });
+   const [userCredentials, setUserCredentials] = useState({
+      email: '',
+      password: '',
+      confirmPassword: '',
+   });
+   const [showPassword, setShowPassword] = useState(false);
 
-    const handleClickShowPassword = useCallback(() => {
-        setShowPassword(prevState => !prevState)
-    }, []);
+   useEffect(() => {
+      setUserDetailsInputs(userDetails);
+   }, [userDetails]);
 
-    const onChangedInputDetails = useCallback((event) => {
-        const {name, value} = event.target;
-        setUserDetailsInputs(prevState => {
-            return {
-                ...prevState,
-                [name]: value
-            }
-        })
-    }, []);
+   const handleClickShowPassword = useCallback(() => {
+      setShowPassword(prevState => !prevState)
+   }, []);
 
-    const onChangedInputCredentials = useCallback((event) => {
-        const {name, value} = event.target;
-        setUserCredentials(prevState => {
-            return {
-                ...prevState,
-                [name]: value
-            }
-        })
-    }, []);
+   const onChangedInputDetails = useCallback((event) => {
+      const {name, value} = event.target;
+      setUserDetailsInputs(prevState => {
+         return {
+            ...prevState,
+            [name]: value
+         }
+      })
+   }, []);
 
-    const onSubmitForm = useCallback((event) => {
-        event.preventDefault();
-        const {confirmPassword, ...userCreds} = userCredentials;
-        let input = {...userDetailsInputs, ...userCreds};
+   const onChangedInputCredentials = useCallback((event) => {
+      const {name, value} = event.target;
+      setUserCredentials(prevState => {
+         return {
+            ...prevState,
+            [name]: value
+         }
+      })
+   }, []);
 
-        if (isEdit && userDetails.userId !== currentUser.userId) {
+   const onSubmitForm = useCallback((event) => {
+      event.preventDefault();
+      if (userCredentials.password !== userCredentials.confirmPassword) {
+         dispatch(setSnackBarStatus({isOpen: true, errorMessage: COMMON_ERROR_MESSAGE_VALIDATE}));
+      } else {
+         const {confirmPassword, ...userCreds} = userCredentials;
+         let input = {...userDetailsInputs, ...userCreds};
+
+         if (isEdit && userDetails.userId !== currentUser.userId) {
             input = userDetailsInputs;
-        }
-        onSubmit(input);
-    }, [isEdit, onSubmit, userCredentials, userDetailsInputs, currentUser, userDetails]);
+         }
+         onSubmit(input);
+      }
+   }, [isEdit, onSubmit, userCredentials, userDetailsInputs, currentUser, userDetails, dispatch]);
 
-    const renderCredentials = useCallback(() => {
-        if (isEdit && (currentUser && userDetails && currentUser.userId !== userDetails.userId)) {
-            return null;
-        }
-        return (
-            <SaveUserCredentials
-                showPassword={showPassword}
-                toggleShowPassword={handleClickShowPassword}
-                onChangedInput={onChangedInputCredentials}
-                credentials={userCredentials}
-            />
-        )
-    }, [
-        currentUser,
-        isEdit,
-        handleClickShowPassword,
-        onChangedInputCredentials,
-        showPassword,
-        userCredentials,
-        userDetails
-    ]);
+   const renderCredentials = useCallback(() => {
+      if (isEdit && (currentUser && userDetails && currentUser.userId !== userDetails.userId)) {
+         return null;
+      }
+      return (
+         <SaveUserCredentials
+            showPassword={showPassword}
+            toggleShowPassword={handleClickShowPassword}
+            onChangedInput={onChangedInputCredentials}
+            credentials={userCredentials}
+         />
+      )
+   }, [
+      currentUser,
+      isEdit,
+      handleClickShowPassword,
+      onChangedInputCredentials,
+      showPassword,
+      userCredentials,
+      userDetails
+   ]);
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline/>
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <PersonAddIcon/>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    {title}
-                </Typography>
-                <form className={classes.form} onSubmit={onSubmitForm}>
-                    <SaveUserDetails
-                        roles={roles}
-                        userDetails={userDetailsInputs}
-                        onChangedInput={onChangedInputDetails}
-                        classes={classes}
-                    />
-                    {renderCredentials()}
-                    <Button
-                        className={classes.submit}
-                        type={"submit"}
-                        variant={"contained"}
-                        color={"primary"}
-                        fullWidth
-                    >{buttonText}</Button>
-                </form>
-            </div>
-        </Container>
-    );
+   return (
+      <Container component="main" maxWidth="xs">
+         <CssBaseline/>
+         <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+               <PersonAddIcon/>
+            </Avatar>
+            <Typography component="h1" variant="h5">
+               {title}
+            </Typography>
+            <form className={classes.form} onSubmit={onSubmitForm}>
+               <SaveUserDetails
+                  roles={roles}
+                  userDetails={userDetailsInputs}
+                  onChangedInput={onChangedInputDetails}
+                  classes={classes}
+               />
+               {renderCredentials()}
+               <Button
+                  className={classes.submit}
+                  type={"submit"}
+                  variant={"contained"}
+                  color={"primary"}
+                  fullWidth
+               >{buttonText}</Button>
+            </form>
+         </div>
+      </Container>
+   );
 };
