9c96155ad703ead63244afd0fd08de8b267c7d87
Iraklii Makarenko <iraklii.makarenko@gmail.com>
2020-03-13 10:55:41 +0200

    refactor code

--- src/view/pages/OrdersPage/OrderListItem/OrderListItem.jsx
+++ src/view/pages/OrdersPage/OrderListItem/OrderListItem.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {
     Grid,
     ListItem,
@@ -7,24 +6,21 @@ import {
 
 import ListItemText from "@material-ui/core/ListItemText";
 
-export const OrderListItem = (props) => {
+const EStatus = {
+    0: 'New',
+    1: 'In Progress',
+    2: 'Ready For Shipping',
+    3: 'Shipping',
+    4: 'Completed',
+    5: 'Cancelled'
+};
 
-    const {
-        order,
-        classes,
-        minWidth350,
-        navigationToOrderDetails
-    } = props;
-
-    const StatusEnum = {
-        0: 'New',
-        1: 'In Progress',
-        2: 'Ready For Shipping',
-        3: 'Shipping',
-        4: 'Completed',
-        5: 'Cancelled'
-    };
-
+export const OrderListItem = ({
+                                  order,
+                                  classes,
+                                  minWidth350,
+                                  navigationToOrderDetails
+                              }) => {
     return (
         <ListItem key={order.orderId} disableGutters divider button
                   onClick={() => navigationToOrderDetails(order.orderId)}>
@@ -36,7 +32,7 @@ export const OrderListItem = (props) => {
                     <ListItemText primary={order.customer.name} secondary={minWidth350 && order.customer.contactEmail}/>
                 </Grid>
                 <Grid item xs={2} className={classes.statusGrid}>
-                    <ListItemText>{StatusEnum[order.status]}</ListItemText>
+                    <ListItemText>{EStatus[order.status]}</ListItemText>
                 </Grid>
             </Grid>
         </ListItem>
