cba8cfaecbb2dec5580077004e9475cb82deb219
VladSegeda <vladcomart@gmail.com>
2020-03-17 11:50:41 +0200

    Localize orders (without statuses)

--- src/view/components/Router/Router.js
+++ src/view/components/Router/Router.js
@@ -63,7 +63,7 @@ export const Routing = () => {
          <Route exact path='/forgot_password' component={ForgotPassword}/>
          <Route exact path='/restore-password/:token' component={RestorePassword}/>
          <Route exact path='/forgot-password' component={ForgotPassword}/>
-         <Route exact path='/new-organization' component={CreateOrganization}/>
+         <Route exact path='/create-organization' component={CreateOrganization}/>
       </Router>
    )
 };
