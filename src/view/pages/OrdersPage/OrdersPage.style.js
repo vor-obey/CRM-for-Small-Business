bf4df23294e00a17c8ec9b829b24ff5f412dd130
borisimo <borisimoprogrammerjs@gmail.com>
2020-03-23 10:43:39 +0200

    feature/dashboard && alignItems

--- src/view/pages/OrdersPage/OrdersPage.style.js
+++ src/view/pages/OrdersPage/OrdersPage.style.js
@@ -1,42 +1,42 @@
 export const ordersPageStyles = ((theme) => ({
-    root: {
-        marginTop: theme.spacing(10),
-        [theme.breakpoints.down('xs')]: {
-            marginTop: theme.spacing(3)
-        }
-    },
-    statusGrid: {
-        display: 'flex',
-        alignItems: 'center',
-        textAlign: 'center',
-        overflowWrap: 'break-word'
-    },
-    customerGrid: {
-        display: 'flex',
-        alignItems: 'center',
-        overflowWrap: 'break-word'
-    },
-    orderGrid: {
-        hyphens: 'auto',
-        overflowWrap: 'break-word'
-    },
-    textStatus: {
-        textAlign: 'center',
-        wordBreak: 'break-all'
-    },
-    searchBox: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'flex-end',
-        [theme.breakpoints.down('xs')]: {
-            justifyContent: 'center',
-        }
-    },
-    search: {
-        width: '300px',
-    },
-    selector: {
-        width: '200px',
-        paddingTop: '2px',
-    },
+   root: {
+      marginTop: theme.spacing(10),
+      [theme.breakpoints.down('xs')]: {
+         marginTop: theme.spacing(3)
+      }
+   },
+   statusGrid: {
+      display: 'flex',
+      alignItems: 'center',
+      textAlign: 'center',
+      overflowWrap: 'break-word'
+   },
+   customerGrid: {
+      display: 'flex',
+      alignItems: 'center',
+      overflowWrap: 'break-word'
+   },
+   orderGrid: {
+      hyphens: 'auto',
+      overflowWrap: 'break-word'
+   },
+   textStatus: {
+      textAlign: 'center',
+      wordBreak: 'break-all'
+   },
+   searchBox: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      [theme.breakpoints.down('xs')]: {
+         justifyContent: 'center',
+      }
+   },
+   search: {
+      width: '300px',
+   },
+   selector: {
+      width: '200px',
+   },
 }));
