3f798d3b228e4808b5f08bbba21719821f1001dd
borisimo <borisimoprogrammerjs@gmail.com>
2020-04-13 15:43:57 +0300

    feature/organization && refactor code input

--- src/view/components/SaveOrganization/SaveOrganizationForm.jsx
+++ src/view/components/SaveOrganization/SaveOrganizationForm.jsx
@@ -4,49 +4,58 @@ import Grid from "@material-ui/core/Grid";
 import {useTranslation} from "react-i18next";
 
 export const SaveOrganizationForm = ({
-                                         onChangedInput,
-                                         organization,
-                                         classes
+                                        onChangedInput,
+                                        organization,
                                      }) => {
-    const { t } = useTranslation('');
+   const {t} = useTranslation('');
 
-    return (
-        <Grid container spacing={2}>
+   const renderCodeInput = () => {
+      if (organization.organizationName && organization.organizationName.length > 0) {
+         return null;
+      } else {
+         return (
             <Grid item xs={12} sm={12}>
-                <TextField
-                    label={t('ORGANIZATION_NAME')}
-                    name="organizationName"
-                    variant="outlined"
-                    type="text"
-                    onChange={onChangedInput}
-                    value={organization.organizationName || ''}
-                    required
-                    fullWidth
-                />
+               <TextField
+                  label={t('CODE')}
+                  name="codeValue"
+                  variant="outlined"
+                  onChange={onChangedInput}
+                  value={organization.codeValue}
+                  required
+                  fullWidth
+               />
             </Grid>
-            <Grid item xs={12} sm={12}>
-                <TextField
-                    label={t('NP_API')}
-                    name="apiKeyNP"
-                    variant="outlined"
-                    type="text"
-                    onChange={onChangedInput}
-                    value={organization.apiKeyNP || ''}
-                    required
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={12} sm={12} className={organization.organizationName ? classes.code: null}>
-                <TextField
-                    label={t('CODE')}
-                    name="codeValue"
-                    variant="outlined"
-                    onChange={onChangedInput}
-                    value={organization.codeValue}
-                    required
-                    fullWidth
-                />
-            </Grid>
-        </Grid>
-    )
+         );
+      }
+   };
+
+   return (
+      <Grid container spacing={2}>
+         <Grid item xs={12} sm={12}>
+            <TextField
+               label={t('ORGANIZATION_NAME')}
+               name="organizationName"
+               variant="outlined"
+               type="text"
+               onChange={onChangedInput}
+               value={organization.organizationName || ''}
+               required
+               fullWidth
+            />
+         </Grid>
+         <Grid item xs={12} sm={12}>
+            <TextField
+               label={t('NP_API')}
+               name="apiKeyNP"
+               variant="outlined"
+               type="text"
+               onChange={onChangedInput}
+               value={organization.apiKeyNP || ''}
+               required
+               fullWidth
+            />
+         </Grid>
+         {renderCodeInput()}
+      </Grid>
+   )
 };
