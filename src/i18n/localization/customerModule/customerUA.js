39321ffca49ad6d009175ee8daaf60bcd55005cc
Iraklii Makarenko <iraklii.makarenko@gmail.com>
2020-04-30 23:34:43 +0300

    fixes

--- src/i18n/localization/customerModule/customerUA.js
+++ src/i18n/localization/customerModule/customerUA.js
@@ -9,5 +9,5 @@ export const CustomerUA = {
     DELETE_CUSTOMER_TEXT: 'Ви впевнені, що хочете видалити клієнта без можливості відновлення?',
     DELETE_CUSTOMER: 'Видалити клієнта',
     EDIT_CUSTOMER: 'Редагувати клієнта',
-    FULL_NAME: 'П.І.Б'
+    FULL_NAME: 'П.І.Б.'
 };
