edc5f67ddfeb8b115f6403034fcad1bb0824118f
VLAD SEGEDA <vladcomart@gmail.com>
2020-05-01 20:34:51 +0300

    Create order -> Add Products & Products List

--- src/view/components/SaveOrderForm/ProductForm/ProductForm.jsx
+++ src/view/components/SaveOrderForm/ProductForm/ProductForm.jsx
@@ -1,21 +1,16 @@
 import React, {useCallback, useEffect, useState} from "react";
-import {Grid, TextField} from "@material-ui/core";
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import {Grid, Button, Typography, Divider, ListItem, IconButton, InputLabel, OutlinedInput, InputAdornment, FormControl} from "@material-ui/core";
 import {closeModal, renderModal} from '../../../../data/store/auxiliary/auxiliaryActions';
 import {useDispatch} from 'react-redux';
 import {AddOrderProduct} from '../../AddOrderProduct/AddOrderProduct';
 import {useProducts} from '../../../../utils/hooks/productHooks';
 import isEmpty from 'lodash/isEmpty';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import CloseIcon from "@material-ui/icons/Close";
-import IconButton from '@material-ui/core/IconButton';
 import RemoveIcon from '@material-ui/icons/Remove';
 import AddIcon from '@material-ui/icons/Add';
 
-export const ProductForm = ({getProducts}) => {
+export const ProductForm = ({getProducts, classes}) => {
+
     const dispatch = useDispatch();
     const [products, setProducts] = useProducts();
     const [selectedProducts, setSelectedProducts] = useState([]);
@@ -107,56 +102,68 @@ export const ProductForm = ({getProducts}) => {
         return selectedProducts.map((item) => {
             const {productId, name, price, amount, totalPrice, currency} = item;
             return (
-                <ListItem key={productId} style={{
-                    border: '1px solid rgba(0, 0, 0, 0.12)',
-                    borderRadius: 5,
-                    marginTop: 5,
-                    marginBottom: 5
-                }}>
-                    <Grid container item xl={12} lg={12}>
-                        <Grid item xl={1} lg={1} style={{
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            textAlign: 'center',
-                            marginTop: 'auto',
-                            marginBottom: 'auto',
-                        }}>
-                            <IconButton onClick={() => removeProduct(item)} size='medium'>
-                                <CloseIcon/>
-                            </IconButton>
-                        </Grid>
-                        <Grid container item xl={11} lg={11}>
-                            <Grid item xl={12} lg={12}>
-                                {name}
-                            </Grid>
-                            <Grid item xl={4} lg={4} style={{
-                                marginTop: 'auto',
-                                marginBottom: 'auto'
-                            }}>
-                                {price} {currency}
-                            </Grid>
-                            <Grid item xl={4} lg={4}>
-                                <IconButton onClick={() => decrement(productId)} disabled={amount === 1}>
-                                    <RemoveIcon/>
+                <ListItem key={productId}
+                          className={classes.productList}>
+                    <Grid container item xs={12} sm={12} className={classes.productContainer}>
+                        <Grid item xs={12} sm={1} className={classes.productContainerItem}>
+                            <Grid className={classes.removeProduct}>
+                                <IconButton onClick={() => removeProduct(item)} size='medium'>
+                                    <CloseIcon/>
                                 </IconButton>
-                                <TextField
-                                    value={amount}
+                            </Grid>
+                        </Grid>
+                        <Grid container item xs={12} sm={6} className={classes.productContainerItem}>
+                            <Grid className={classes.productTitle}>
+                                <Typography variant='body1' className={classes.productTitleName}>
+                                    {name}
+                                </Typography>
+                                <Typography variant='body1'>
+                                    {price} {currency}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} sm={2} className={classes.productContainerItem}>
+                            <FormControl variant="outlined" fullWidth>
+                                <InputLabel>Amount</InputLabel>
+                                <OutlinedInput
+                                    className={classes.amount}
+                                    type='text'
                                     label='Amount'
                                     name='amount'
-                                    type='text'
-                                    variant='outlined'
+                                    value={amount}
                                     onChange={(event) => onAmountChange(event.target.value, productId)}
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <IconButton
+                                                className={classes.amountButton}
+                                                fontSize="small"
+                                                onClick={() => decrement(productId)} disabled={amount === 1}>
+                                                <RemoveIcon/>
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                className={classes.amountButton}
+                                                fontSize="small"
+                                                onClick={() => increment(productId)}>
+                                                <AddIcon/>
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    labelWidth={70}
                                 />
-                                <IconButton onClick={() => increment(productId)}>
-                                    <AddIcon/>
-                                </IconButton>
-                            </Grid>
-                            <Grid item xl={4} lg={4} style={{textAlign: 'center'}}>
-                                <Typography variant='body1' style={{
-                                    display: 'flex',
-                                    flexDirection: 'column'
-                                }}>
-                                    <span style={{fontWeight: 'bold'}}>Summary:</span> {totalPrice} {currency}
+                            </FormControl>
+
+                        </Grid>
+                        <Grid item xs={12} sm={2} className={classes.productContainerItem}>
+                            <Grid className={classes.productContainerTotal}>
+                                <Typography variant='subtitle1'>
+                                    Summary:
+                                </Typography>
+                                <Typography variant='h6'>
+                                    {totalPrice} {currency}
                                 </Typography>
                             </Grid>
                         </Grid>
@@ -164,38 +171,39 @@ export const ProductForm = ({getProducts}) => {
                 </ListItem>
             );
         })
-    }, [selectedProducts, onAmountChange, decrement, increment, removeProduct]);
+    }, [classes, selectedProducts, onAmountChange, decrement, increment, removeProduct]);
 
     return (
         <>
-            <Grid item xl={12} lg={12}>
+            <Grid item xs={12} sm={12}>
                 <Typography variant='h6'>
                     Product(s)
                 </Typography>
                 <Divider/>
             </Grid>
-            <List style={{width: '100%'}}>
+            <Grid container item xs={12} sm={12}>
                 {renderSelectedProducts()}
-                <ListItem style={{paddingLeft: 0, paddingRight: 0}}>
-                    <Grid item xl={12} lg={12} style={{marginTop: 10}}>
-                        <Button
-                            variant='outlined'
-                            onClick={() => openAddOrderProductModal()}
-                        >
+                <Grid className={classes.productContainerMeta}>
+                    <Grid item xs={12} sm={6}>
+                        <Button variant='outlined'
+                                onClick={() => openAddOrderProductModal()}>
                             Add product
                         </Button>
                     </Grid>
-                </ListItem>
-                {!isEmpty(selectedProducts) ? (
-                    <ListItem>
-                        <Grid container item xl={12} lg={12} style={{textAlign: 'right'}}>
-                            <Typography variant='body1'>
-                                <span style={{fontWeight: 'bold'}}>Total:</span> {calculateTotalPoints()}
-                            </Typography>
-                        </Grid>
-                    </ListItem>
-                ) : null}
-            </List>
+                    <Grid item xs={12} sm={6} className={classes.productContainerTotal}>
+                        {!isEmpty(selectedProducts) ? (
+                            <>
+                                <Typography variant='subtitle1'>
+                                    Total:
+                                </Typography>
+                                <Typography variant='h6'>
+                                    {calculateTotalPoints()}
+                                </Typography>
+                            </>
+                        ) : null}
+                    </Grid>
+                </Grid>
+            </Grid>
         </>
     );
 };
