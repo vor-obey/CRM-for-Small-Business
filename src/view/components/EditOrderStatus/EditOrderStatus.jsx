eedf89595a6e456fe7cc249eff0e19c8f9cedb3e
VLAD SEGEDA <vladcomart@gmail.com>
2020-05-18 15:21:16 +0300

    revert changes with select

--- src/view/components/EditOrderStatus/EditOrderStatus.jsx
+++ src/view/components/EditOrderStatus/EditOrderStatus.jsx
@@ -1,16 +1,10 @@
 import React, {useCallback, useEffect, useState} from "react";
 import {
-    Grid,
     FormControl,
-    IconButton,
     Select,
     InputLabel,
-    Typography,
     makeStyles
 } from "@material-ui/core";
-import RestoreIcon from '@material-ui/icons/Restore';
-import EditIcon from "@material-ui/icons/Edit";
-import CheckIcon from "@material-ui/icons/Check";
 import {EOrderStatus} from "../../../constants/statuses";
 import {editOrderStatusStyles} from "./EditOrderStatus.style";
 import {useTranslation} from "react-i18next";
@@ -25,30 +19,12 @@ export function EditOrderStatus({
     useEffect(() => {
         setCurrentStatus(status);
     }, [status]);
-
-    const [newStatus, setNewStatus] = useState();
-    const [isEditStatus, setIsEditStatus] = useState(true);
     const classes = useStyle();
     const {t} = useTranslation('');
 
-    const editStatus = useCallback(() => {
-        setIsEditStatus(prevState => !prevState);
-    }, [])
-
-    const restoreStatus = useCallback(() => {
-        setIsEditStatus(prevState => !prevState);
-        setNewStatus(undefined);
-    }, [])
-
     const onStatusSelectHandler = useCallback((value) => {
-        setNewStatus(value);
-    }, [setNewStatus]);
-
-    const saveStatus = useCallback(() => {
-        setCurrentStatus(newStatus);
-        submit(newStatus);
-        setIsEditStatus(prevState => !prevState);
-    }, [newStatus, submit]);
+        submit(value);
+    }, [submit]);
 
     const renderStatuses = useCallback(() => {
         const entries = Object.entries(EOrderStatus);
@@ -57,57 +33,18 @@ export function EditOrderStatus({
         });
     }, [t]);
 
-    const renderActionButton = useCallback(() => {
-        if (isEditStatus) {
-            return (
-                <IconButton onClick={editStatus}>
-                    <EditIcon className={classes.editButton} color={'secondary'}/>
-                </IconButton>
-            )
-        }
-        return (
-            <IconButton onClick={restoreStatus}>
-                <RestoreIcon className={classes.editButton} color={'secondary'}/>
-            </IconButton>
-        )
-    }, [classes, editStatus, restoreStatus, isEditStatus]);
-
-    const renderAddButton = useCallback(() => {
-        if (isEditStatus) {
-            return null
-        }
-        return (
-            <Grid className={classes.actionADD}>
-                <IconButton onClick={saveStatus}>
-                    <CheckIcon className={classes.editButton} color={'secondary'}/>
-                </IconButton>
-            </Grid>
-        )
-    }, [classes, isEditStatus, saveStatus]);
-
-    const renderField = useCallback(() => {
-        if (isEditStatus) {
-            return (
-                <Grid className={classes.currentStatus}>
-                    <Typography
-                        variant='body1'>
-                        {t(EOrderStatus[currentStatus])}
-                    </Typography>
-                </Grid>
-            )
-        }
-        return (
+    return (
             <FormControl variant="filled" size={"small"} margin={'none'}>
-                <InputLabel>{'Select status'}</InputLabel>
+                <InputLabel>{t('STATUS')}</InputLabel>
                 <Select
-                    label={'Select status'}
+                    label={t('STATUS')}
                     classes={{
                         disabled: classes.selectStatusDisabled,
                         filled: classes.selectStatusFilled,
                     }}
                     native
                     name="status"
-                    value={newStatus || (currentStatus && currentStatus)}
+                    value={(currentStatus && currentStatus)}
                     onChange={(event) => onStatusSelectHandler(event.target.value)}
                     inputProps={{
                         name: 'status',
@@ -115,18 +52,5 @@ export function EditOrderStatus({
                     {renderStatuses()}
                 </Select>
             </FormControl>
-        )
-    }, [classes, isEditStatus, onStatusSelectHandler, renderStatuses, currentStatus, newStatus, t]);
-
-    return (
-        <Grid item xs={12} sm={12} className={classes.orderItem}>
-            {renderAddButton()}
-            <Grid className={classes.statusForm}>
-                {renderField()}
-            </Grid>
-            <Grid className={classes.actionButtonPanel}>
-                {renderActionButton()}
-            </Grid>
-        </Grid>
     )
 }
